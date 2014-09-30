a0cd2658867bea8d722278e6e6928c8320811fbd
Steven Zeiler <zeiler.steven@gmail.com>
2014-09-30 12:26:39 -0700

    [TASK] Fix transactions.list and test.

--- test/client.js
+++ test/client.js
@@ -1,19 +1,24 @@
-var CoinbaseClient = require(__dirname + '/../lib/client.js');
+const CoinbaseClient = require(__dirname + '/../lib/client.js');
+const assert = require('assert');
 
-var client = new CoinbaseClient({
+const client = new CoinbaseClient({
   apiKey: process.env.COINBASE_API_KEY,
   secret: process.env.COINBASE_SECRET_KEY
 });
 
 describe('Coinbase Node Service', function () {
 
-  it('should make a request to Coinbase and log out the response or error', function () {
+  it('should make a request to Coinbase and log out the response or error', function (done) {
     client.list()
-      .then(function(response) {
-        console.log(response);
+      .then(function(transactions) {
+        assert(transactions.length > 0);
+        console.log(transactions);
+        done();
       })
       .error(function(error) {
-        console.log(error);
+        console.log('ERROR', error);
+        assert(false);
+        done();
       });
   });
 
