05c67bedd68bc25ae1a329a6fce7170d8df361c9
Abiy Seifu <abiy@ripple.com>
2014-09-30 18:38:46 -0700

    [FEATURE] export plugin and routes

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const QuotesController = require(__dirname+'/lib/controllers/quotes_controller');
+
+function CoinbasePlugin(options) {
+  var router = new express.Router();
+
+
+  var quotesController = new QuotesController ({
+    gatewayd: options.gatewayd
+  });
+
+  router.get('/v1', function(request, response) {
+    response.send({
+      success: true,
+      plugin: {
+        name: 'gatewayd-coinbase',
+        version: '0.1.0',
+        documentation: 'https://github.com/gatewayd/coinbase-plugin'
+      }
+    })
+  });
+
+  router.get('/v1/payments/quotes', quotesController.test.bind(quotesController));
+
+
+  this.router = router;
+  this.processes = {
+    outgoing : __dirname+'/processes/outgoing'
+  };
+
+
+};
+
+module.exports = CoinbasePlugin;
