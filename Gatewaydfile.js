039d4cc33502215c90145af14efcfd1e01b4132a
Abiy Seifu <abiy@ripple.com>
2014-10-01 11:15:02 -0700

    [TASK] pass api key / secret

--- Gatewaydfile.js
+++ Gatewaydfile.js
@@ -3,9 +3,12 @@ var CoinbasePlugin = require('coinbase-plugin');
 module.exports = function(gatewayd) {
 
   var coinbasePlugin = new CoinbasePlugin({
-    gatewayd: gatewayd
+    gatewayd: gatewayd,
+    apiKey: '',
+    secret: ''
   });
 
   gatewayd.processes.add(coinbasePlugin.processes.outgoing);
+  gatewayd.server.use('/coinbase', coinbasePlugin.router);
 
 };
